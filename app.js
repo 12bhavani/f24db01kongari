5553152daed11731d09347b1126e0dc6af13dbd2
Bhavani Kongari <s575201@nwmissouri.edu>
2024-11-13 19:31:46 -0600

    updated changes

--- app.js
+++ app.js
@@ -8,8 +8,6 @@ require('dotenv').config();
 const connectionString = process.env.MONGO_CON
 mongoose = require('mongoose');
 mongoose.connect(connectionString, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
   serverSelectionTimeoutMS: 10000,
 })
 .then(() => console.log("MongoDB connected successfully!"))
